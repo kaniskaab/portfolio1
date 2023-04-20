75ebaddea0be94169cbd746a2ada13e8e522b02a
Kaniskaa Behera <101510957+kaniskaab@users.noreply.github.com>
2023-04-20 14:21:31 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,16 +17,16 @@ export default function Contact() {
       <input type="hidden" name="access_key" value="e5aee2aa-94e5-4c42-8882-d147ecd5dc5b" /> 
       <div className="grid gap-6 sm:grid-cols-2">
         <div className="relative z-0">
-          <input type="text" name="name" className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-[#26a75d] focus:border-[#26a75d] focus:outline-none focus:ring-0" placeholder=" " />
-          <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-[#26a75d] peer-focus:dark:text-[#26a75d]">Your name</label>
+          <input type="text" name="name" className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-2xl text-[#26a75d] focus:border-[#26a75d] focus:outline-none focus:ring-0" placeholder=" " />
+          <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-xl text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-[#26a75d] peer-focus:dark:text-[#26a75d]">Your name</label>
         </div>
         <div className="relative z-0">
-          <input type="text" name="email" className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm focus:border-[#26a75d] focus:outline-none focus:ring-0 text-[#26a75d]" placeholder=" " />
-          <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-[#26a75d] peer-focus:dark:text-[#26a75d]">Your email</label>
+          <input type="text" name="email" className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-2xl focus:border-[#26a75d] focus:outline-none focus:ring-0 text-[#26a75d]" placeholder=" " />
+          <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-xl text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-[#26a75d] peer-focus:dark:text-[#26a75d]">Your email</label>
         </div>
         <div className="relative z-0 col-span-2">
-          <textarea name="message" rows="5" className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-[#26a75d] focus:border-[#26a75d] focus:outline-none focus:ring-0" placeholder=" "></textarea>
-          <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-[#26a75d] peer-focus:dark:text-[#26a75d]">Your message</label>
+          <textarea name="message" rows="5" className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-2xl text-[#26a75d] focus:border-[#26a75d] focus:outline-none focus:ring-0" placeholder=" "></textarea>
+          <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-xl text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-[#26a75d] peer-focus:dark:text-[#26a75d]">Your message</label>
         </div>
       </div>
       <button type="submit" class="mt-5 rounded-md bg-[#26a75d] px-10 py-2 text-black">Send Message</button>
